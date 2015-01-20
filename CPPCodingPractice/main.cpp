6f409ffb241b211fc1531a492cf3b7ac97544cc4
projectchewie <projectchewie@gmail.com>
2015-01-20 17:46:57 -0500

    Initial Commit

---
+++ CPPCodingPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPCodingPractice
+//
+//  Created by Jonathan Chen on 1/20/15.
+//  Copyright (c) 2015 Jonathan Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
